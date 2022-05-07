152c7fc7c8debfed1ef64d3de35679cb1bc81f8e
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2022-05-07 16:06:33 -0300

    first commit

---
+++ TestApplicationMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestApplicationMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
